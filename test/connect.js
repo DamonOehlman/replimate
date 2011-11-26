1dbd3b952ca7de0755c8d010d10f4ab6b6ef0ce0
Damon Oehlman <damon.oehlman@gmail.com>
2011-11-26 12:04:56 +1000

    Skeleton of lib coming together

---
+++ test/connect.js
@@ -0,0 +1,10 @@
+var replimate = require('../'),
+    config = require('config');
+
+describe('connectivity', function() {
+    it('should be able to connect to the _replicator database', function(done) {
+        replimate(config.couchurl, function(err, res) {
+            done(err);
+        });
+    });
+});
